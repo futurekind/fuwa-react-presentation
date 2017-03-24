9d14251f9d12ab10293a76421d75ade6d1de5f6a
Fabian Trinler <fabian.trinler@blateral.com>
2017-03-24 13:07:39 +0100

    Adding basic theme for slides

---
+++ presentation/slides/Start.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Slide, Heading } from 'spectacle';
+
+export default class extends React.Component {
+    render() {
+        return (
+            <Slide bgColor="dark">
+                <Heading>Hey</Heading>
+            </Slide>
+        )
+    }
+}
+    
